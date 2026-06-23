9705755d92cad3bfe734699f212024dc560573dc
yshtysht8sh10-glitch <yshtysht8sh10@gmail.com>
2026-06-23 23:04:31 +0900

    feat: expose loaded character palettes

--- src/core/character/CharacterTypes.ts
+++ src/core/character/CharacterTypes.ts
@@ -4,10 +4,17 @@ import type { CmdDocument } from '../../parser/cmd/CmdTypes';
 import type { DefDocument } from '../../parser/def/DefTypes';
 import type { ImageDataSpritePack } from '../sprite/ImageDataSpriteTypes';
 
+export type CharacterPaletteAsset = {
+  slot: number;
+  file: string;
+  bytes: Uint8Array;
+};
+
 export type CharacterAssets = {
   def: DefDocument;
   cns: CnsDocument;
   air: AirDocument;
   cmd: CmdDocument;
   sprites: ImageDataSpritePack | null;
+  palettes: CharacterPaletteAsset[];
 };
